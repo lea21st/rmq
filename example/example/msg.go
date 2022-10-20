06ac6e3836a3d40e4977e8df2199d888cd54a784
pong <lea21st@qq.com>
2022-10-20 09:47:13 +0800

    add test task hook

--- example/example/msg.go
+++ example/example/msg.go
@@ -27,6 +27,20 @@ type TestTask struct {
 	Val  int    `json:"val"`
 }
 
+func (t *TestTask) Success(ctx context.Context) {
+	fmt.Println(t.Name, "success hook")
+}
+
+func (t *TestTask) Fail(ctx context.Context) {
+	// TODO implement me
+	fmt.Println(t.Name, "Fail hook")
+}
+
+func (t *TestTask) Complete(ctx context.Context) {
+	// TODO implement me
+	fmt.Println(t.Name, "Complete hook")
+}
+
 func (t *TestTask) TaskName() string {
 	// TODO implement me
 	return "TestTask"
